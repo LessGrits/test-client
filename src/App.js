5238e952f2adb4e8add89018a7f220399b2478cc
Oles <ruboken@gmail.com>
2020-07-05 19:23:52 +0300

    created template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+            <img src="https://image.freepik.com/free-vector/_98292-3951.jpg" alt="logo"/>
+        </div>
+        <button className="new-hotdog">
+          Add hotdog
+        </button>
       </header>
+      <main>
+        <h1>All hot-dogs</h1>
+        <section className="main-content">
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="hotdog">
+            <div className="hotdog__img">
+              <img src="https://mesta.com.ua/wp-content/uploads/2019/11/2-40.png" alt="hotdog"/>
+              <h3 className="hotdog__name">
+                Chicago
+              </h3>
+              <p className="hotdog__price">1$</p>
+              <p className="hotdog__description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, doloremque doloribus eius esse, est
+                explicabo iure maxime minus neque nisi nostrum officia perspiciatis praesentium provident qui quibusdam
+                vitae voluptas voluptatum.
+                Asperiores at, autem consectetur harum id illo ipsa iure iusto nam necessitatibus nostrum provident
+                quaerat, quas quasi quos repudiandae sint suscipit tempora tempore temporibus unde ut voluptate. Dolore,
+                maiores, quas.
+              </p>
+              <button className="hotdog__edit-btn">
+                Edit
+              </button>
+            </div>
+          </div>
+
+        </section>
+      </main>
+      <footer>
+        &copy;Footer
+      </footer>
     </div>
   );
 }
