b5ef0c6d230744c42a5885a5ec16cfa7d97f730a
Oles <ruboken@gmail.com>
2020-07-07 15:52:19 +0300

    Updates

---
+++ precache-manifest.ffa3812d8260cde9a11d21b4d0268f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d04d045c9d127ce24d3263fc22e217",
+    "url": "/test-client/index.html"
+  },
+  {
+    "revision": "4722a27e4dd13fb11f84",
+    "url": "/test-client/static/css/main.b0910374.chunk.css"
+  },
+  {
+    "revision": "064a59d04f8c8b3acd6a",
+    "url": "/test-client/static/js/2.6b0528bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-client/static/js/2.6b0528bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4722a27e4dd13fb11f84",
+    "url": "/test-client/static/js/main.bdd8327d.chunk.js"
+  },
+  {
+    "revision": "a09287ad4e958551d136",
+    "url": "/test-client/static/js/runtime-main.88efa1db.js"
+  }
+]);
